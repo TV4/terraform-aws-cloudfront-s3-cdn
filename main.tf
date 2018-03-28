687340cec31fa9c01c7bd95d7a3b65ee92248709
Anders Wintergren <anders.wintergren@bonnierbroadcasting.com>
2018-03-28 15:55:47 +0200

    testing

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_cloudfront_distribution" "default" {
   }
 
   viewer_certificate {
-    acm_certificate_arn            = "${var.acm_certificate_arn == "" ? var.acm_certificate_arn : aws_acm_certificate_validation.cert.certificate_arn}"
+    acm_certificate_arn            = "${var.acm_certificate_arn == "" ? var.acm_certificate_arn : element(aws_acm_certificate_validation.cert.*.certificate_arn, 0)}"
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1"
     cloudfront_default_certificate = false
