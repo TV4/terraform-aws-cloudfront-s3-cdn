d16832b26fd0e08888d1c6e1151b67b933fb2d80
Anders Wintergren <anders.wintergren@bonnierbroadcasting.com>
2018-03-29 10:56:40 +0200

    testing

--- main.tf
+++ main.tf
@@ -183,7 +183,7 @@ data "aws_route53_zone" "zone" {
 resource "aws_route53_record" "cert_validation" {
   count = "${length(var.aliases)}"
   name = "${aws_acm_certificate.cert.domain_validation_options.*.resource_record_name}"
-  type = "${aws_acm_certificate.cert.domain_validation_options.*.resource_record_type}"
+  type = "CNAME"
   zone_id = "${data.aws_route53_zone.zone.id}"
   records = ["${aws_acm_certificate.cert.domain_validation_options.*.resource_record_value}"]
   ttl = 60
