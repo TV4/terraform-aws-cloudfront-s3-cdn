d5e7381e629ceea462ee1c712330d78f881b354d
Anders Wintergren <anders.wintergren@bonnierbroadcasting.com>
2018-03-29 10:28:15 +0200

    testing

--- main.tf
+++ main.tf
@@ -181,10 +181,11 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  count = "${length(var.aliases)}"
+  name = "${aws_acm_certificate.cert.domain_validation_options.*.resource_record_name}"
+  type = "${aws_acm_certificate.cert.domain_validation_options.*.resource_record_type}"
   zone_id = "${data.aws_route53_zone.zone.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  records = ["${aws_acm_certificate.cert.domain_validation_options.*.resource_record_value}"]
   ttl = 60
 }
 
